92e341e0f1c5b4b10bc55e06565d61948aad37a7
Xeoncross <david@xeoncross.com>
2009-03-22 19:28:40 -0600

    Updated the documentation

--- themes/default/welcome/welcome.php
+++ themes/default/welcome/welcome.php
@@ -1,27 +1,14 @@
 <h2>Welcome to Micro Model-View-Controller</h2>
 <b>Thank you for trying out MicroMVC</b>
 
-<p>Micro Model-View-Controller (or MicroMVC) is a tiny MVC framework for 
-building websites with PHP 5. It offers Model, Library, Controller, Function, 
-and View management. Plus other features like URI Routing, hooks/plugins, 
-file uploads, database ORM, captcha creation, and more! If you are new 
-the Model/View/Controller world of OOP - then this system is for you. 
-Every line is documented making it simple to tear-apart and learn from.</p>
+<p>This is a full MVC framework crammed into less than 90kb - 
+including documentation! I built this to have a base to compare 
+the speed of other frameworks like CodeIgniter, Zend, Solar, and CakePHP.</p>
 
-<p>MicroMVC is not a replacement for a full MCV framework. Do not plan on
-building the next Facebook with it! This system is built for small teams
- that need the power of Object Oriented Design without the "multiple 
- server cost" of large bloated systems. MicroMVC has three goals:</p>
-
-<ul>
-<li>Teach new programers how MVC works with a simple-to-understand code structure.</li>
-<li>Show how fast a PHP system can be to encourage other projects to increase performace.</li>
-<li>Run small systems (like blogs and article sites) that need MVC without a lot of overhead.</li>
-</ul>
-
-<p>MicroMVC is licensed under the <a href="http://www.gnu.org/licenses/gpl-3.0.html">GPL v3.0</a>
-so you can use it for any personal or corporate projects free of charge. 
-We believe in open source, so if you see something that is being done 
-inefficiently - let us know so we can fix it.</p>
+<p>It offers Model, Library, Controller, function, and View management. 
+Plus other features like URI Routing, file uploads, database abstraction, 
+captcha creation, and more! If you are new the Model/View/Controller world 
+of OOP - then this system is for you. Every line is documented and it 
+is as simple of a system to tear-apart as you can get.</p>
 
 <?php include('debug.php'); ?>
